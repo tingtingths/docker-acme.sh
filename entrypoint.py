1b6e81783586a932bde4142aaa3d0b3f87520936
Tony Ting <k999hoshing@gmail.com>
2021-03-24 15:08:46 +0800

    fix: run weekly.

--- entrypoint.py
+++ entrypoint.py
@@ -3,13 +3,16 @@ import schedule
 import sys
 import time
 import subprocess
+from datetime import date
 
 
 def runner():
     subprocess.run(['/usr/bin/env', 'bash', '/runner.sh'] + sys.argv[1:])
+    next_run = schedule.next_run().strftime('%Y-%m-%d %H:%M:%S')
+    print(f'Next run at [{next_run}]')
 
 # setup jobs
-schedule.every().sunday.do(runner)
+schedule.every().week.do(runner)
 # print all jobs
 [print(f'JOB - {str(job)}') for job in schedule.jobs]
 # run now
